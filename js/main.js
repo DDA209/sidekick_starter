aec4b5c78b7ab2cadfe52807774b52eda60eb279
DDA209 <didier.jeux1@gmail.com>
2020-07-05 00:29:34 +0200

    work on prices

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+import counterUp from'counterup2'
+/*
+counterUp(document.querySelector('.counter'){
+    duration: 1000,
+    delay: 16
+})
+//*/  
+
+//*
+const el = document.querySelector('.counter')
+new Waypoint({
+    element: el,
+    handler: function() { 
+      counterUp( el );
+      this.destroy();
+    },
+    offset: 'bottom-in-view'
+});
+//*/
